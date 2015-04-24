8cc780c47fa6d5dcb95931e9995845ca4387538c
pushkaraj <pushkarajvs@gmail.com>
2015-04-25 00:30:48 +0530

    Added a normalized grid as per Tanmoy's suggestion

--- main.py
+++ main.py
@@ -105,7 +105,7 @@ class game(object):
         else:
             self.owner_of_next_line=self.owner_of_next_line
             self.owner_of_last_line=self.owner_of_next_line
-        
+
     def player_next_to(self,player):
         return (player+1)%self.no_of_players
 
@@ -226,6 +226,44 @@ class game(object):
 
     def fill_box(self,box,owner_of_the_box)
         pass
+
+class norm_grid(object):
+    def __init__(self,grid_size):
+        self.grid_size=grid_size
+        self.grid=
+
+    def not_adjacent(self,dot1,dot2):
+        if(self.dot_above(dot1) != dot2 and self.dot_below(dot1) != dot2 and self.dot_to_left_of(dot1) != dot2 and
+                self.dot_to_right_of(dot1) != dot2)
+            return True
+        else:
+            return False
+
+    def dot_to_right_of(self,current_dot):
+        if(self.is_rightmost(current_dot)):
+            return None
+        else:
+            return(dot(current_dot.get_x()+self.dot_separation,current_dot.get_y()))
+
+    def dot_to_left_of(self,current_dot):
+        if(self.is_leftmost(current_dot)):
+            return None
+        else:
+            return(dot(current_dot.get_x()-self.dot_separation,current_dot.get_y()))
+
+    def dot_above(self,current_dot):
+        if(self.is_topmost(current_dot)):
+            return None
+        else:
+            return(dot(current_dot.get_x(),current_dot.get_y()+self.dot_separation))
+
+    def dot_below(self,current_dot):
+        if(self.is_bottommost(current_dot)):
+            return None
+        else:
+            return(dot(current_dot.get_x(),current_dot.get_y()-self.dot_separation))
+
+
 class grid(object):
     def __init__(self,left_top,grid_size,dot_size):
         self.left_top_dot=dot(left_top)
