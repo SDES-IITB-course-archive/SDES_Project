b1774d73747814170c03228cde81db2906130243
Tanmoy Khan <tanmoykhans@gmail.com>
2015-04-25 21:27:10 +0530

    Finding selected line algorithm changed

--- createGrid.py
+++ createGrid.py
@@ -37,26 +37,72 @@ class Grid(object):
       def displayGrid(self):
           cv2.imshow("grid window",self.grid)
           
+      def detectHorizontalLine(self,x,y):
+             i=0
+             noPossibleLine=True
+             while i<=self.row-1:
+               if y>self.dotPosList[i][0][1]-self.dotRadius and y<self.dotPosList[i][0][1]+self.dotRadius:
+                  noPossibleLine=False
+                  break;
+               i+=1
+             if noPossibleLine==True:
+                return None
+             j=0
+             while j<=self.col-2:
+               if x>self.dotPosList[0][j][0]+self.dotRadius and x<self.dotPosList[0][j+1][0]-self.dotRadius:
+                  return (i,j)
+             return None
       
-      def detectClosestNode(dotPosList,pos):             
-          nodes = np.asarray(dotPosList)
-          dist_2 = np.sum((nodes - pos)**2, axis=1)
-          closestNode=np.argmin(dist_2)
-          y=closestNode/self.col
-          x=closestNode%self.row
-          return (x,y)
-          
-      def detectLine(x,y):
-          if y<self.dotPosList[0][0][1]-self.dotRadius or y>self.dotPosList[self.row][0][1]+self.dotRadius \
-          or x<self.dotPosList[0][0][0]-self.dotRadius or x>self.dotPosList[0][self.col][0]+self.dotRadius:
-             return -1
-          else:
-             y<self.dotPosList[0][0][1]+self.dotPosList:
-
+      def detectVerticalLine(self,x,y):
+             j=0
+             noPossibleLine=True
+             while j<=self.col-1:
+               if x>self.dotPosList[0][j][0]-self.dotRadius and x<self.dotPosList[0][j][0]+self.dotRadius:
+                  noPossibleLine=False
+                  break;
+               j+=1
+             if noPossibleLine==True:
+                return 	None
+             i=0
+             while i<=self.col-2:
+               if y>self.dotPosList[i][0][1]+self.dotRadius and y<self.dotPosList[i+1][0][1]-self.dotRadius:
+                  return (i,j)
+             return None
+      
+      def isOutsideArea(self,x,y):
+           if y<=self.dotPosList[0][0][1]-self.dotRadius or y>=self.dotPosList[self.row-1][0][1]+self.dotRadius \
+           or x<=self.dotPosList[0][0][0]-self.dotRadius or x>=self.dotPosList[0][self.col-1][0]+self.dotRadius:
+             return True
+           else:
+             return False
+             
+      def lineSelected(self,x,y):
+           if self.isOutsideArea(x,y):
+              return None
+           line=self.detectHorizontalLine(x,y)
+           if line!=None:
+              self.lastSelectedLine=[line,(line[0],line[1]+1)]
+              return self.lastSelectedLine
+           line=self.detectVerticalLine(x,y)
+           if line!=None:
+              self.lastSelectedLine=[line,(line[0]+1,line[1])]
+              return self.lastSelectedLine
+           return None
+           
+           
+       #draws line between dot1 and dot2    
+      def drawLine(self,dot1,dot2,color=(255,255,255)):
+           print self.dotPosList[dot1[0]][dot1[1]], self.dotPosList[dot2[0]][dot2[1]]
+           cv2.line(self.grid, self.dotPosList[dot1[0]][dot1[1]], self.dotPosList[dot2[0]][dot2[1]], color, thickness=40)
+       
+      def drawLastSelectedLine(self):
+           self.drawLine(self.lastSelectedLine)
+           
 if __name__=="__main__":
    grid =Grid(6,6,8,40)
    grid.displayGrid()
-   print grid.dotPosList
+   print grid.dotPosList[0][0]
+   grid.drawLine((0,0),(0,1))   
    key = cv2.waitKey()
    if key == 27:
      cv2.destroyWindow("grid window")
